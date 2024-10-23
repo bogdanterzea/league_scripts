cfe8d328aa291707731b667657f4cb3623d1c081
bogdanterzea <terzeabogdan2007@yahoo.com>
2024-10-23 16:16:24 +0300

    Create multiple solutions to be tested

--- accept_game/accept.py
+++ accept_game/accept.py
@@ -16,9 +16,48 @@ def checkGameAvailable():
         pos = imagesearch(acceptButtonImg, 0.8)
         if not pos[0] == -1:
             pyautogui.moveTo(pos[0]+30, pos[1]+30)
-            time.sleep(0.5)
-            pyautogui.click()
-            pyautogui.click()
+
+            # #Solution 0 ------------------------------------------------------------------------------------------
+            # time.sleep(0.5)
+            # pyautogui.click()
+            # pyautogui.click()
+
+            # # Solution 1 - enter before clicking ------------------------------------------------------------------------------------------
+            # pyautogui.press('enter')  # Simulate pressing Enter key
+            # pyautogui.click()
+
+            # # Solution 3 ---------------------------------------------------------------------------------------------------------------------------------------
+            # time.sleep(1)
+            # pyautogui.click()
+            # time.sleep(1)
+
+            # # Solution 4 - other library ------------------------------------------------------------------------------------------
+            # # pip install pydirectinput
+            # # import pydirectinput
+            # pydirectinput.click()
+
+            # # Solution 5 ---------------------------------------------------------------------------------------------------------------------------------------
+            # # pip install pywin32
+            # # import win32api, win32con
+            # # import time
+            # def click_at(x, y):
+            # # Move the mouse to the specified position
+            # win32api.SetCursorPos((x, y))
+            
+            # # Simulate a left mouse button down and up (click)
+            # win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
+            # time.sleep(0.5)
+            # win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
+
+            # # Solution 6 ---------------------------------------------------------------------------------------------------------------------------------------
+            # # pip install PyDirectInput
+            # # import pydirectinput
+            # # import time
+
+            # def click_at(x, y):
+            #     pydirectinput.moveTo(x, y)
+            #     pydirectinput.click()
+
             print("Game accepted!")
             break
         time.sleep(TIMELAPSE)
